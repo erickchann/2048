d3ed037a38e6f3edb6417330f13bc9d5e4b9bea2
erickchann <erickchandra718@gmail.com>
2022-10-09 19:51:51 +0700

    feat: mobile middleware

--- script.js
+++ script.js
@@ -1,264 +1,272 @@
-const row1 = document.querySelectorAll('.row1');
-const row2 = document.querySelectorAll('.row2');
-const row3 = document.querySelectorAll('.row3');
-const row4 = document.querySelectorAll('.row4');
+const row1 = document.querySelectorAll(".row1");
+const row2 = document.querySelectorAll(".row2");
+const row3 = document.querySelectorAll(".row3");
+const row4 = document.querySelectorAll(".row4");
 
-const modal = document.querySelector('.modal');
-const modalWin = document.querySelector('.modal-win');
-const modalOver = document.querySelector('.modal-over');
+const modal = document.querySelector(".modal");
+const modalWin = document.querySelector(".modal-win");
+const modalOver = document.querySelector(".modal-over");
 
-const scr = document.getElementById('score');
-const bst = document.getElementById('best');
+const scr = document.getElementById("score");
+const bst = document.getElementById("best");
 let score = 0;
-let best = parseInt(localStorage.getItem('best')) || 0;
+let best = parseInt(localStorage.getItem("best")) || 0;
 
 const colors = {
-	'0' : '#cdc1b4',
-	'2' : '#eee4da',
-	'4' : '#eee1c9',
-	'8' : '#f3b27a',
-	'16' : '#f69664',
-	'32' : '#f77c5f',
-	'64' : '#f75f3b',
-	'128' : '#edd073',
-	'256' : '#edcc62',
-	'512' : '#f4b136',
-	'1024' : '#daff00',
-	'2048' : '#59ff00'
+    0: "#cdc1b4",
+    2: "#eee4da",
+    4: "#eee1c9",
+    8: "#f3b27a",
+    16: "#f69664",
+    32: "#f77c5f",
+    64: "#f75f3b",
+    128: "#edd073",
+    256: "#edcc62",
+    512: "#f4b136",
+    1024: "#daff00",
+    2048: "#59ff00",
 };
 
 const grid = [
-	[0,0,0,0],
-	[0,0,0,0],
-	[0,0,0,0],
-	[0,0,0,0]
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
 ];
 
-const dom = [
-	row1,
-	row2,
-	row3,
-	row4
-];
+const dom = [row1, row2, row3, row4];
 
+window.onload = function () {
+    const a = navigator.userAgent || navigator.vendor || window.opera;
+    if (
+        /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
+            a
+        ) ||
+        /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+            a.substr(0, 4)
+        )
+    ) {
+        document.body.style.width = "100vw";
+        document.body.style.height = "100vh";
+        document.body.style.overflow = "hidden";
+        document.getElementById("only-desktop-overlay").style.display = "flex";
+    }
+};
 
 function init() {
-	modal.classList.add('close');
+    modal.classList.add("close");
 
-	bst.innerHTML = best;
+    bst.innerHTML = best;
 
-	addNumber();
-	addNumber();
+    addNumber();
+    addNumber();
 }
 
 function addNumber() {
-	let options = [];
+    let options = [];
 
-	for (let i = 0; i < 4; i++) {
-		for (let j = 0; j < 4; j++) {
-			if (grid[i][j] == 0) {
-				options.push({
-					x: i,
-					y: j
-				});
-			}
-		}
-	}
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (grid[i][j] == 0) {
+                options.push({
+                    x: i,
+                    y: j,
+                });
+            }
+        }
+    }
 
-	if (isWin()) {
-		modalWin.style.display = 'block';
-	}
+    if (isWin()) {
+        modalWin.style.display = "block";
+    }
 
-	if (isGameOver()) {
-		if (score > best) {
-			localStorage.setItem('best', score);
-		}
+    if (isGameOver()) {
+        if (score > best) {
+            localStorage.setItem("best", score);
+        }
 
-		modalOver.style.display = 'block';
-	}
+        modalOver.style.display = "block";
+    }
 
-	if (options.length != 0) {
-		let rand = options[Math.floor(Math.random() * options.length)];
-		grid[rand.x][rand.y] = 2;
-		draw();
-	}
+    if (options.length != 0) {
+        let rand = options[Math.floor(Math.random() * options.length)];
+        grid[rand.x][rand.y] = 2;
+        draw();
+    }
 }
 
 function draw() {
-	for (let i = 0; i < 4; i++) {
-		for (let j = 0; j < 4; j++) {
-			if (grid[i][j] != 0) {
-				dom[i][j].innerHTML = grid[i][j];
-				dom[i][j].style.backgroundColor = colors[grid[i][j]];
-			} else {
-				dom[i][j].innerHTML = '';
-				dom[i][j].style.backgroundColor = colors[grid[i][j]];
-			}
-		}		
-	}
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (grid[i][j] != 0) {
+                dom[i][j].innerHTML = grid[i][j];
+                dom[i][j].style.backgroundColor = colors[grid[i][j]];
+            } else {
+                dom[i][j].innerHTML = "";
+                dom[i][j].style.backgroundColor = colors[grid[i][j]];
+            }
+        }
+    }
 }
 
 function isWin() {
-	for (let i = 0; i < 4; i++) {
-		for (let j = 0; j < 4; j++) {
-			if (grid[i][j] == 2048) {
-				return true;
-			}
-		}
-	}
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (grid[i][j] == 2048) {
+                return true;
+            }
+        }
+    }
 
-	return false;
+    return false;
 }
 
 function isGameOver() {
-	for (let i = 0; i < 4; i++) {
-		for (let j = 0; j < 4; j++) {
-			if (grid[i][j] == 0) {
-				return false;
-			}
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            if (grid[i][j] == 0) {
+                return false;
+            }
 
-			if (i != 3 && j != 3) {
-				if (grid[i][j] == grid[i][j + 1]) {
-					return false;
-				}
+            if (i != 3 && j != 3) {
+                if (grid[i][j] == grid[i][j + 1]) {
+                    return false;
+                }
 
-				if (grid[i][j] == grid[i + 1][j]) {
-					return false;
-				}
-			}
+                if (grid[i][j] == grid[i + 1][j]) {
+                    return false;
+                }
+            }
 
-			if (i != 0 && j != 0) {
-				if (grid[i][j] == grid[i][j - 1]) {
-					return false;
-				}
+            if (i != 0 && j != 0) {
+                if (grid[i][j] == grid[i][j - 1]) {
+                    return false;
+                }
 
-				if (grid[i][j] == grid[i - 1][j]) {
-					return false;
-				}
-			}
+                if (grid[i][j] == grid[i - 1][j]) {
+                    return false;
+                }
+            }
+        }
+    }
 
-		}
-	}
-
-	return true;
+    return true;
 }
 
 function updateScore(val) {
-	score += val;
+    score += val;
 
-	scr.innerHTML = score;
+    scr.innerHTML = score;
 }
 
-document.addEventListener('keydown', (e) => {
-	switch(e.keyCode) {
+document.addEventListener("keydown", (e) => {
+    switch (e.keyCode) {
+        // LEFT
+        case 37:
+            for (var a = 0; a < 4; a++) {
+                for (let i = 0; i < 4; i++) {
+                    for (let j = 0; j < 4; j++) {
+                        if (j != 0) {
+                            // Collapse
+                            if (grid[i][j] == grid[i][j - 1]) {
+                                updateScore((grid[i][j - 1] *= 2));
 
-		// LEFT
-		case 37:
-			for (var a = 0; a < 4; a++) {
-				for (let i = 0; i < 4; i++) {
-					for (let j = 0; j < 4; j++) {
-						if (j != 0) {
-							// Collapse
-							if (grid[i][j] == grid[i][j - 1]) {
-								updateScore(grid[i][j - 1] *= 2);
+                                grid[i][j - 1] = grid[i][j] * 2;
+                                grid[i][j] = 0;
+                            }
 
-								grid[i][j - 1] = grid[i][j] * 2;
-								grid[i][j] = 0;
-							}
+                            // Move
+                            if (grid[i][j - 1] == 0) {
+                                grid[i][j - 1] = grid[i][j];
+                                grid[i][j] = 0;
+                                draw();
+                            }
+                        }
+                    }
+                }
+            }
+            addNumber();
+            break;
 
-							// Move
-							if (grid[i][j - 1] == 0) {
-								grid[i][j - 1] = grid[i][j];
-								grid[i][j] = 0;
-								draw();
-							}
-						}
-					}
-				}
-			}
-			addNumber();
-		break;
+        // RIGHT
+        case 39:
+            for (var a = 0; a < 4; a++) {
+                for (let i = 0; i < 4; i++) {
+                    for (let j = 0; j < 4; j++) {
+                        if (j != 3) {
+                            // Collapse
+                            if (grid[i][j] == grid[i][j + 1]) {
+                                updateScore((grid[i][j + 1] *= 2));
 
-		// RIGHT
-		case 39:
-			for (var a = 0; a < 4; a++) {
-				for (let i = 0; i < 4; i++) {
-					for (let j = 0; j < 4; j++) {
-						if (j != 3) {
-							// Collapse
-							if (grid[i][j] == grid[i][j + 1]) {
-								updateScore(grid[i][j + 1] *= 2);
+                                grid[i][j + 1] = grid[i][j] * 2;
+                                grid[i][j] = 0;
+                            }
 
-								grid[i][j + 1] = grid[i][j] * 2;
-								grid[i][j] = 0;
-							}
+                            // Move
+                            if (grid[i][j + 1] == 0) {
+                                grid[i][j + 1] = grid[i][j];
+                                grid[i][j] = 0;
+                                draw();
+                            }
+                        }
+                    }
+                }
+            }
+            addNumber();
+            break;
 
-							// Move
-							if (grid[i][j + 1] == 0) {
-								grid[i][j + 1] = grid[i][j];
-								grid[i][j] = 0;
-								draw();
-							}
-						}
-					}
-				}
-			}
-			addNumber();
-		break;
+        // UP
+        case 38:
+            for (var a = 0; a < 4; a++) {
+                for (let i = 0; i < 4; i++) {
+                    for (let j = 0; j < 4; j++) {
+                        if (i != 0) {
+                            // Collapse
+                            if (grid[i][j] == grid[i - 1][j]) {
+                                updateScore((grid[i - 1][j] *= 2));
 
-		// UP
-		case 38:
-			for (var a = 0; a < 4; a++) {
-				for (let i = 0; i < 4; i++) {
-					for (let j = 0; j < 4; j++) {
-						if (i != 0) {
-							// Collapse
-							if (grid[i][j] == grid[i - 1][j]) {
-								updateScore(grid[i - 1][j] *= 2);
+                                grid[i - 1][j] = grid[i][j] * 2;
+                                grid[i][j] = 0;
+                            }
 
-								grid[i - 1][j] = grid[i][j] * 2;
-								grid[i][j] = 0;
-							}
+                            // Move
+                            if (grid[i - 1][j] == 0) {
+                                grid[i - 1][j] = grid[i][j];
+                                grid[i][j] = 0;
+                                draw();
+                            }
+                        }
+                    }
+                }
+            }
+            addNumber();
+            break;
 
-							// Move
-							if (grid[i - 1][j] == 0) {
-								grid[i - 1][j] = grid[i][j];
-								grid[i][j] = 0;
-								draw();
-							}
-						}
-					}
-				}
-			}
-			addNumber();
-		break;
+        // Down
+        case 40:
+            for (var a = 0; a < 4; a++) {
+                for (let i = 0; i < 4; i++) {
+                    for (let j = 0; j < 4; j++) {
+                        if (i != 3) {
+                            // Collapse
+                            if (grid[i][j] == grid[i + 1][j]) {
+                                updateScore((grid[i + 1][j] *= 2));
 
-		// Down
-		case 40:
-			for (var a = 0; a < 4; a++) {
-				for (let i = 0; i < 4; i++) {
-					for (let j = 0; j < 4; j++) {
-						if (i != 3) {
-							// Collapse
-							if (grid[i][j] == grid[i + 1][j]) {
-								updateScore(grid[i + 1][j] *= 2);
+                                grid[i + 1][j] = grid[i][j] * 2;
+                                grid[i][j] = 0;
+                            }
 
-								grid[i + 1][j] = grid[i][j] * 2;
-								grid[i][j] = 0;
-							}
-
-							// Move
-							if (grid[i + 1][j] == 0) {
-								grid[i + 1][j] = grid[i][j];
-								grid[i][j] = 0;
-								draw();
-							}
-						}
-					}
-				}
-			}
-			addNumber();
-		break;
-
-	}
+                            // Move
+                            if (grid[i + 1][j] == 0) {
+                                grid[i + 1][j] = grid[i][j];
+                                grid[i][j] = 0;
+                                draw();
+                            }
+                        }
+                    }
+                }
+            }
+            addNumber();
+            break;
+    }
 });
